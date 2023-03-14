bdff707fcd2becc20fe075dd8ab10c3cc6399bc4
zmrx <zmrxqq@gmail.com>
2023-03-14 14:56:56 +0700

    fix two_sum

--- src/6kyu/Two_Sum/index.ts
+++ src/6kyu/Two_Sum/index.ts
@@ -1,11 +1,21 @@
+type twoSumResultItem = [number, number];
+
+/**
+ * 1. генерируем новый массив, со всеми возможными вариантами.
+ * 2. фильтруем варианты с разными значениями (условие задачи).
+ * 3. фильтруем варианты, суммирование которых дает нужный результат.
+ * 4. возвращаем первый результат.
+ */
 export const twoSum = (numbers: number[], target: number) =>
   numbers
     .reduce(
-      (arr, _, index1) => [
-        ...arr,
-        ...numbers.map((_, index2) => [index1, index2]),
-      ],
-      [] as number[][]
+      (arr, _, index1) =>
+        [
+          ...arr,
+          ...numbers.map((_, index2) => [index1, index2]),
+        ] as twoSumResultItem[],
+
+      [] as twoSumResultItem[]
     )
     .filter((item) => item[0] !== item[1])
     .filter((item) => numbers[item[0]] + numbers[item[1]] === target)
